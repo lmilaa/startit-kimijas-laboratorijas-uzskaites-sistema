c367b2d233f6c92330761282f69719a89a5828d7
lmilaa <ludmilabuivide@gmail.com>
2020-12-29 09:20:42 +0000

    izveidot route sveiki

--- serveris.py
+++ serveris.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 def index():
     return "Labrīt!"
 
-
+@app.route('/sveiki')
+def sveiki():
+  return"Nav vairs nekāds rīts!"
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
